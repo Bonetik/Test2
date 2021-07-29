b42eae61da7c6b820acc246720158179bdfdbd67
Андрей <79835601+Bonetik@users.noreply.github.com>
2021-07-29 12:44:01 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -33,8 +33,4 @@ $(document).ready(function () {
       });
     });
   });
-  var scene = document.getElementById('scene');
-  var parallaxInstance = new Parallax(scene, {
-    relativeInput: true
-  });
 });
